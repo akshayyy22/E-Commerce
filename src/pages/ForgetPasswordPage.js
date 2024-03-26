fb498df7f0da862659208e18b0bf221aa4a8c41e
akshayyy22 <akshaypersonalstudentid@gmail.com>
2024-03-26 22:36:24 +0530

    Cleanup Of Front-End Code

--- src/pages/ForgetPasswordPage.js
+++ src/pages/ForgetPasswordPage.js
@@ -1,5 +1,4 @@
 import ForgotPassword from "../features/auth/components/ForgetPassword";
-import Login from "../features/auth/components/Login";
 function ForgotPasswordPage() {
     return ( <div>
         <ForgotPassword></ForgotPassword>
